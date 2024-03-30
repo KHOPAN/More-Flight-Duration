c58a8789aea32884dbba41f203f74725f196c25e
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-03-30 17:42:48 +0700

    Add Matches Logic

--- src/main/java/com/khopan/moreflightduration/recipe/FireworkRocketUpgradeRecipe.java
+++ src/main/java/com/khopan/moreflightduration/recipe/FireworkRocketUpgradeRecipe.java
@@ -1,6 +1,7 @@
 package com.khopan.moreflightduration.recipe;
 
 import net.minecraft.core.RegistryAccess;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
@@ -12,7 +13,8 @@ import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.Level;
 
 public class FireworkRocketUpgradeRecipe extends CustomRecipe {
-	private static final Ingredient TNT_INGREDIENT = Ingredient.of(Items.TNT);
+	private static final Ingredient FIREWORK_ROCKET_INGREDIENT = Ingredient.of(Items.FIREWORK_ROCKET);
+	private static final Ingredient GUNPOWDER_INGREDIENT = Ingredient.of(Items.GUNPOWDER);
 
 	public FireworkRocketUpgradeRecipe(ResourceLocation location, CraftingBookCategory category) {
 		super(location, category);
@@ -20,16 +22,51 @@ public class FireworkRocketUpgradeRecipe extends CustomRecipe {
 
 	@Override
 	public boolean matches(CraftingContainer container, Level level) {
-		if(container.getContainerSize() < 1) {
+		ItemStack fireworkRocket = null;
+		int fireworkRocketCount = 0;
+		int gunpowderCount = 0;
+
+		for(int i = 0; i < container.getContainerSize(); i++) {
+			ItemStack stack = container.getItem(i);
+
+			if(stack == null || stack.isEmpty()) {
+				continue;
+			}
+
+			if(FireworkRocketUpgradeRecipe.FIREWORK_ROCKET_INGREDIENT.test(stack)) {
+				if(fireworkRocket != null && !ItemStack.isSameItemSameTags(fireworkRocket, stack)) {
+					return false;
+				}
+
+				fireworkRocket = stack;
+				fireworkRocketCount++;
+			} else if(FireworkRocketUpgradeRecipe.GUNPOWDER_INGREDIENT.test(stack)) {
+				gunpowderCount++;
+			} else {
+				return false;
+			}
+		}
+
+		if((fireworkRocketCount != 1 && fireworkRocketCount != 3) || gunpowderCount < 1) {
 			return false;
 		}
 
-		return FireworkRocketUpgradeRecipe.TNT_INGREDIENT.test(container.getItem(0));
+		CompoundTag tag = fireworkRocket.getTag();
+
+		if(tag.contains("Fireworks")) {
+			CompoundTag fireworks = tag.getCompound("Fireworks");
+
+			if(fireworks.contains("Flight") && fireworks.getByte("Flight") + gunpowderCount * (fireworkRocketCount == 1 ? 3 : 1) > Byte.MAX_VALUE) {
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 	@Override
 	public ItemStack assemble(CraftingContainer container, RegistryAccess access) {
-		return new ItemStack(Items.DIAMOND, 1);
+		return new ItemStack(Items.FIREWORK_ROCKET, 1);
 	}
 
 	@Override
@@ -39,7 +76,7 @@ public class FireworkRocketUpgradeRecipe extends CustomRecipe {
 
 	@Override
 	public ItemStack getResultItem(RegistryAccess registryAccess) {
-		return new ItemStack(Items.DIAMOND);
+		return new ItemStack(Items.FIREWORK_ROCKET);
 	}
 
 	@Override
