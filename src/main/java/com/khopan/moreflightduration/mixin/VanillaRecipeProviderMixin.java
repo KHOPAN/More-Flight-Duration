2a960a7897cae117e4539345c8e9369f62c09918
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-03-30 13:06:59 +0700

    Add VanillaRecipeProviderMixin

---
+++ src/main/java/com/khopan/moreflightduration/mixin/VanillaRecipeProviderMixin.java
@@ -0,0 +1,22 @@
+package com.khopan.moreflightduration.mixin;
+
+import java.util.function.Consumer;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.khopan.moreflightduration.recipe.RecipeSerializerRegistry;
+
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.SpecialRecipeBuilder;
+import net.minecraft.data.recipes.packs.VanillaRecipeProvider;
+
+@Mixin(VanillaRecipeProvider.class)
+public abstract class VanillaRecipeProviderMixin {
+	@Inject(method="buildRecipes(Ljava/util/function/Consumer;)V", at=@At("TAIL"))
+	private void buildRecipesMixin(CallbackInfo info, Consumer<FinishedRecipe> writer) {
+		SpecialRecipeBuilder.special(RecipeSerializerRegistry.FIREWORK_ROCKET_UPGRADE).save(writer, "firework_rocket_upgrade");
+	}
+}
