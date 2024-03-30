b7956d037e8a4413681916f3cf2f5e90eed18dbb
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-03-30 13:10:45 +0700

    Test Recipe

--- src/main/java/com/khopan/moreflightduration/recipe/FireworkRocketUpgradeRecipe.java
+++ src/main/java/com/khopan/moreflightduration/recipe/FireworkRocketUpgradeRecipe.java
@@ -4,24 +4,32 @@ import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.CraftingBookCategory;
 import net.minecraft.world.item.crafting.CustomRecipe;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.Level;
 
 public class FireworkRocketUpgradeRecipe extends CustomRecipe {
+	private static final Ingredient TNT_INGREDIENT = Ingredient.of(Items.TNT);
+
 	public FireworkRocketUpgradeRecipe(ResourceLocation location, CraftingBookCategory category) {
 		super(location, category);
 	}
 
 	@Override
 	public boolean matches(CraftingContainer container, Level level) {
-		return false;
+		if(container.getContainerSize() < 1) {
+			return false;
+		}
+
+		return FireworkRocketUpgradeRecipe.TNT_INGREDIENT.test(container.getItem(0));
 	}
 
 	@Override
 	public ItemStack assemble(CraftingContainer container, RegistryAccess access) {
-		return null;
+		return new ItemStack(Items.DIAMOND, 1);
 	}
 
 	@Override
