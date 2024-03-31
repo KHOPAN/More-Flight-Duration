59defa3f3b6909bad15dece6cd92ba99924d35ca
KHOPAN <93379212+KHOPAN@users.noreply.github.com>
2024-03-31 15:42:40 +0700

    Port to Forge

--- src/main/java/com/khopan/moreflightduration/MoreFlightDuration.java
+++ src/main/java/com/khopan/moreflightduration/MoreFlightDuration.java
@@ -3,16 +3,16 @@ package com.khopan.moreflightduration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.fabricmc.api.ModInitializer;
+import net.minecraftforge.fml.common.Mod;
 
-public class MoreFlightDuration implements ModInitializer {
+@Mod(MoreFlightDuration.MOD_ID)
+public class MoreFlightDuration {
 	public static final String MOD_NAME = "More Flight Duration";
 	public static final String MOD_ID = "moreflightduration";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MoreFlightDuration.MOD_NAME);
 
-	@Override
-	public void onInitialize() {
+	public MoreFlightDuration() {
 		MoreFlightDuration.LOGGER.info("Initializing {}", MoreFlightDuration.MOD_NAME);
 	}
 }
